e0aef3e22d8b88fe32fd6b9fb57994fd011d3af5
norihumi <norihumiy03@gmail.com>
2026-09-10 17:36:09 +0900

    02nd : メインビジュアル

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// =======================================
+// ハンバーガーメニュー
+// =======================================
 const hamburger = document.querySelector('.header__hamburger');
 const nav = document.querySelector('.header__nav');
 const navLinks = document.querySelectorAll('.header__item a');
@@ -13,3 +16,23 @@ navLinks.forEach((link)=>{
         nav.classList.remove('is-open');
     })
 })
+
+// =======================================
+// メインビジュアル スライドショー
+// =======================================
+const slides = document.querySelectorAll('.mv__slide');
+let currentSlide = 0;
+
+function nextSlide() {
+    // 現在のスライドから is-active を外す（非表示へ）
+    slides[currentSlide].classList.remove('is-active');
+    
+    // 次のスライド番号へ（最後の次は0に戻る）
+    currentSlide = (currentSlide + 1) % slides.length;
+    
+    // 次のスライドに is-active をつける（表示へ）
+    slides[currentSlide].classList.add('is-active');
+}
+
+// 4000ミリ秒（4秒）ごとに nextSlide を実行
+setInterval(nextSlide, 4000);
